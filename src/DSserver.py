05200c8b22f209d68cec471b4643b0bd4cda72de
Powerrrrrr <710233268@qq.com>
2019-01-02 22:48:46 +0800

    modify DSserver.py

--- src/DSserver.py
+++ src/DSserver.py
@@ -5,37 +5,35 @@ import socket
 import DSquery
 import DSsocket
 import subprocess
-PORT = 12345
-BUF_SIZE = 4096
 
-def callGrepOnVM(grepCall):
-	pattern = grepCall.split(" ")
-	pattern.insert(0,u'grep')
-	pattern.insert(1,u'-n')
+_port = 12345
+_buffer_size = 4096
 
+
+def call_grep_cmd(command: str) -> bytes:
 	try:
-		output = subprocess.check_output(pattern).decode('utf-8').strip()
-		output = str(output)
-		output = output.split("\n")
-		for i in range(0, len(output)):
-			yield output[i] + '\n'
-	except subprocess.CalledProcessError as e:
-		if e.returncode  == 1:
-			yield
-		elif e.returncode == 2:
-			yield
+		result = subprocess.check_output(command, shell=True)
+		result = str(output.decode('utf-8'))
+		output_list = list(filter(result.split("\n")))
+		for item in output_list:
+			yield item + '\n'
+	except subprocess.CalledProcessError as error:
+		if error.returncode == 1:
+
 
 if __name__ == "__main__":
 	server = TCPSocket()
-	server.bind(('', PORT))
+	server.bind(('', _port))
 	server.listen(10)
+
 	while True:
-		client = server.accept()
-		msg = client.sock.recv(BUF_SIZE)
-		grep_cmd = msg.decode('utf-8')
-		for output in callGrepOnVM(grep_cmd):
+		client, client_info = server.accept()
+		message = client.sock.recv(_buffer_size)
+		grep_cmd = message.decode('utf-8')
+		for output in call_grep_cmd(grep_cmd):
 			try:
-				c.send(output.encode())
+				print(output)
+				client.send(output.encode())
 			except:
 				pass
-		c.close()
+		client.close()
