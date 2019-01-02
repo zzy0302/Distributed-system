c132e3843fa4fbb6f78340edb2ffdaf3a627af42
zzy0302 <503652983@qq.com>
2019-01-02 20:58:17 +0800

    fix Query.py

--- src/DSquery.py
+++ src/DSquery.py
@@ -1,49 +1,8 @@
 import re
 import sys
-import subprocess
 import copy
+import subprocess
 
-def doQuery(pattern, filename):
-    pattern2 = pattern
-    if pattern2[0:2] != '.*':
-        pattern2 = '.*' + pattern2
-    if pattern2[-2:] != '.*':
-        pattern2 = pattern2 + '.*'
-    try:
-        re_obj = re.compile(pattern2)
-    except:
-        print("Invalid query pattern: %s" %pattern)
-        return
-    f = open(filename, 'r')
-    result = []
-    for idx, line in enumerate(f.readlines()):
-        line = line.strip()
-        m = re_obj.match(line)
-        if m:
-            output = str(idx) + ': ' + line
-            result.append(output)
-    f.close()
-    return '\n'.join(result)
-
-def doQuery2(pattern, filename):
-    pattern2 = pattern
-    if pattern2[0:2] != '.*':
-        pattern2 = '.*' + pattern2
-    if pattern2[-2:] != '.*':
-        pattern2 = pattern2 + '.*'
-    try:
-        re_obj = re.compile(pattern2)
-    except:
-        print("Invalid query pattern: %s" %pattern)
-        return
-    f = open(filename, 'r')
-    for idx, line in enumerate(f.readlines()):
-        line = line.strip()
-        m = re_obj.match(line)
-        if m:
-            output = str(idx) + ': ' + line + '\n'
-            yield output
-    f.close()
 
 
 def callGrepOnVM(grepCall):
@@ -55,8 +14,6 @@ def callGrepOnVM(grepCall):
 		output = subprocess.check_output(pattern).decode('utf-8').strip()
 		output = str(output)
 		output = output.split("\n")
-		# print(output)
-		# print(len(output))
 		for i in range(0, len(output)):
 			yield output[i] + '\n'
 	except subprocess.CalledProcessError as e:
