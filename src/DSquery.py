04b5a09eedbfe677d2b891d3ede1d887cecadffb
zzy0302 <503652983@qq.com>
2019-01-02 21:43:18 +0800

    fix socket bug

--- src/DSquery.py
+++ src/DSquery.py
@@ -3,53 +3,6 @@ import sys
 import copy
 import subprocess
 
-<<<<<<< HEAD
-
-def doQuery(pattern, filename):
-    pattern2 = pattern
-    if pattern2[0:2] != '.*':
-        pattern2 = '.*' + pattern2
-    if pattern2[-2:] != '.*':
-        pattern2 = pattern2 + '.*'
-    try:
-        re_obj = re.compile(pattern2)
-    except:
-        print("Invalid query pattern: %s" %pattern)
-        return
-    f = open(filename, 'r')
-    result = []
-    for idx, line in enumerate(f.readlines()):
-        line = line.strip()
-        m = re_obj.match(line)
-        if m:
-            output = str(idx) + ': ' + line
-            result.append(output)
-    f.close()
-    return '\n'.join(result)
-
-
-def doQuery2(pattern, filename):
-    pattern2 = pattern
-    if pattern2[0:2] != '.*':
-        pattern2 = '.*' + pattern2
-    if pattern2[-2:] != '.*':
-        pattern2 = pattern2 + '.*'
-    try:
-        re_obj = re.compile(pattern2)
-    except:
-        print("Invalid query pattern: %s" %pattern)
-        return
-    f = open(filename, 'r')
-    for idx, line in enumerate(f.readlines()):
-        line = line.strip()
-        m = re_obj.match(line)
-        if m:
-            output = str(idx) + ': ' + line + '\n'
-            yield output
-    f.close()
-=======
->>>>>>> 0fae74eba307dd5e638d3b56089d6ec10316097d
-
 
 def callGrepOnVM(grepCall):
 	pattern = grepCall.split(" ")
--- src/DSserver.py
+++ src/DSserver.py
@@ -5,24 +5,7 @@ import DSsocket
 
 PORT = 12345
 BUF_SIZE = 4096
-<<<<<<< HEAD
 
-
-def parser_msg(msg):
-	msg_d = msg.decode()
-	pattern = msg_d.split(' ')[0]
-	filename = msg_d.split(' ')[1]
-	return pattern, filename
-
-
-def parser_grep(grepcmd):
-	grepcmd_d = grepcmd.decode('utf-8')
-	return grepcmd_d
-								
-
-=======
-				
->>>>>>> 0fae74eba307dd5e638d3b56089d6ec10316097d
 if __name__ == "__main__":
 	server = TCPSocket()
 	server.bind(('', PORT))
