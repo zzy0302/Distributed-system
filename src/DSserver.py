248be6ed6e9de21e5ca4a260b96af16cce90f2c3
zzy0302 <503652983@qq.com>
2019-01-02 21:48:51 +0800

    remove DSquery

--- src/DSserver.py
+++ src/DSserver.py
@@ -1,10 +1,29 @@
+import re
 import sys
+import copy
 import socket
 import DSquery
 import DSsocket
-
+import subprocess
 PORT = 12345
 BUF_SIZE = 4096
+
+def callGrepOnVM(grepCall):
+	pattern = grepCall.split(" ")
+	pattern.insert(0,u'grep')
+	pattern.insert(1,u'-n')
+
+	try:
+		output = subprocess.check_output(pattern).decode('utf-8').strip()
+		output = str(output)
+		output = output.split("\n")
+		for i in range(0, len(output)):
+			yield output[i] + '\n'
+	except subprocess.CalledProcessError as e:
+		if e.returncode  == 1:
+			yield
+		elif e.returncode == 2:
+			yield
 
 if __name__ == "__main__":
 	server = TCPSocket()
