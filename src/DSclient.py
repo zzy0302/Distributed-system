3a8bdafec87857efe5956c6b4d9a493a115f2338
zzy0302 <503652983@qq.com>
2019-01-03 08:07:13 +0800

    modified

--- src/DSclient.py
+++ src/DSclient.py
@@ -12,22 +12,16 @@ _message_length = 4096
 
 def node_process(pattern: str, nodes: dict) -> dict:
 	global _server_port
-	# print("2")
 	params = {'buffer': '', 'complete': False, 'count': 0}
 	for node in nodes:
 		node.update(params)
 		try:
 			node['sock'] = TCPSocket()
 			node['sock'].connect((node['ip'], _server_port))
-			# pattern_copy = copy.deepcopy(pattern)
-			print('pattern: ', pattern)
 			pattern_copy = pattern
-			print('pattern_copy: ', pattern_copy)
 			message = ' '.join(pattern_copy)
-			print(message)
 			node['sock'].send(message)
 			node['status'] = True
-			# print("3")
 		except ConnectionRefusedError as e:
 			node['status'] = False
 			node['complete'] = True
@@ -46,7 +40,6 @@ def node_detected(node: dict, mode: int) -> dict:
 					return node
 				node['buffer'] += chunk
 				records = node['buffer'].split('\n')
-				print('records: ', records)
 				for i in range(len(records) - 1):
 					if mode == 0:
 						print(node['name'] + ': ' + records[i])
@@ -61,70 +54,6 @@ def node_detected(node: dict, mode: int) -> dict:
 			node['status'] = False
 			node['complete'] = True
 			return node
-
-
-
-def new_connect_to_server(pattern, filename='config.json', mode=0):
-	global _message_length
-	with open(filename,'r') as file_obj:
-		nodes = json.loads(file_obj.read())
-	params = {'buffer': '', 'complete': False, 'count': 0}
-	for node in nodes:
-		node.update(params)
-		try:
-			node['sock'] = TCPSocket()
-			node['sock'].connect((node['ip'], _server_port))
-			# pattern_copy = copy.deepcopy(pattern)
-			print('pattern: ', pattern)
-			pattern_copy = pattern
-			print('pattern_copy: ', pattern_copy)
-			message = ' '.join(pattern_copy)
-			print(message)
-			node['sock'].send(message)
-			node['status'] = True
-			# print("3")
-		except ConnectionRefusedError as e:
-			node['status'] = False
-			node['complete'] = True
-	while True:
-		for node in nodes:
-			if node['status'] and not node['complete']:
-				try:
-					if node['sock'].activityDetected(5):
-						chunk = node['sock'].recv(_message_length)
-						print(chunk)
-						if chunk == '':
-							node['complete'] = True
-							# return node
-							continue
-						node['buffer'] += chunk
-						records = node['buffer'].split('\n')
-						print('records: ', records)
-						for i in range(len(records) - 1):
-							if mode == 0:
-								print(node['name'] + ': ' + records[i])
-							node['count'] += 1
-						node['buffer'] = records[-1]
-						# return node
-					else:
-						node['complete'] = True
-						# return node
-				except ConnectionRefusedError as e:
-					print(str(e) + ': ' + node['name'])
-					node['status'] = False
-					node['complete'] = True
-					# return node
-			# node = node_detected(node, mode)
-		if functools.reduce((lambda x,y: x and y), [node['complete'] for node in nodes]):
-			for node in nodes:
-				# print("5")
-				if not node['status']:
-					print(node['name'] + 'caught an error.')
-				else:
-					print(node['name'] + " completed with " + str(node['count']) + ' lines.')
-			break
-
-	return nodes
 
 
 def connect_to_server(pattern, filename='config.json', mode=0):
@@ -156,4 +85,3 @@ if __name__ == "__main__":
 		print("end")
 		end = time.time()
 		print("Query time: %.4fs" %(end - start))
-		pass
